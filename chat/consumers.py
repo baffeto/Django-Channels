cf9cdc3144ab257831bb81ace466b055e79998be
baffeto <baffeto@mail.ru>
2023-08-27 13:52:17 +0300

    Created receive method (consumers.py) + updated template

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,9 +3,22 @@ from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        # Тут мы отправляем, а в lobby.html обрабатываем
         self.accept()
         
         self.send(text_data=json.dumps({
             'type': 'connection_established',
             'message': 'You are now connected!'
+        }))
+        
+    def receive(self, text_data):
+        # Здесь мы получаем message из form lobby.html
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        
+        print('Message: ', message)
+        
+        self.send(text_data=json.dumps({
+            'type': 'chat',
+            'message': message
         }))
