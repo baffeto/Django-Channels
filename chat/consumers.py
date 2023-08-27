c227a625ef0c83a021e4eb324c2f4ed2b9ffa97a
baffeto <baffeto@mail.ru>
2023-08-27 15:05:21 +0300

    Optimized code (async_to_sync)

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,22 +1,48 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         # Тут мы отправляем, а в lobby.html обрабатываем
+        self.room_group_name = 'test'
+        
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        
         self.accept()
         
-        self.send(text_data=json.dumps({
-            'type': 'connection_established',
-            'message': 'You are now connected!'
-        }))
+        
+        # self.send(text_data=json.dumps({
+        #     'type': 'connection_established',
+        #     'message': 'You are now connected!'
+        # }))
         
     def receive(self, text_data):
         # Здесь мы получаем message из form lobby.html
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         
-        print('Message: ', message)
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+        
+        # print('Message: ', message)
+        
+        # self.send(text_data=json.dumps({
+        #     'type': 'chat',
+        #     'message': message
+        # }))
+        
+    def chat_message(self, event):
+        message = event['message']
         
         self.send(text_data=json.dumps({
             'type': 'chat',
